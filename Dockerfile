e7651edcbf5bd59a33c812708d2950e30ade72ce
Ke Jin <jack.kejin@gmail.com>
2017-11-29 20:28:19 -0500

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM nginx:alpine
+# Setup proxy server
+COPY proxy.conf /etc/nginx/conf.d/proxy.conf
+COPY ipquery.txt /etc/nginx/html/163/ipquery.txt
+RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+
+# If you want to add a homepage for your server:
+# Name it index.html, put it in same directory as Dockerfile,
+# and uncomment next line
+# COPY index.html /usr/share/nginx/html/index.html
+
+# Setup DNS and PAC
+RUN apk --no-cache add bind-tools dnsmasq
+EXPOSE 53 53/udp
+COPY prepare-files.sh /etc/prepare-files.sh
+RUN /bin/sh -c 'sh /etc/prepare-files.sh'
+
+CMD dnsmasq & nginx
---
+++ prepare-files.sh
@@ -0,0 +1,32 @@
+publicIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+echo "no-resolv
+server=8.8.8.8
+server=208.67.222.222
+address=/music.163.com/${publicIP}
+address=/ip.ws.126.net/${publicIP}
+address=/music.httpdns.c.163.com/${publicIP}
+address=/m10.music.126.net/125.39.1.27
+" >> /etc/dnsmasq.conf
+
+echo "function FindProxyForURL(url, host) {
+	if (host == 'music.163.com' || host == 'ip.ws.126.net') {
+		return 'PROXY ${publicIP}';
+	} else if (host == 'music.httpdns.c.163.com') {
+		return 'PROXY 127.0.0.1:9999';
+	} else if (host == 'm10.music.126.net') {
+		return 'PROXY 125.39.1.27:80';
+	}
+	return 'DIRECT';
+}" >> /usr/share/nginx/html/proxy.pac
+
+echo "server {
+  listen       80;
+  listen       [::]:80;
+  server_name  music.httpdns.c.163.com;
+  
+  location /d {
+    default_type application/json;
+    return 200 '{\"dns\":[{\"host\":\"music.httpdns.c.163.com\",\"ips\":[\"${publicIP}\"],\"ttl\":36000,\"http2\":0},{\"host\":\"music.163.com\",\"ips\":[\"${publicIP}\"],\"ttl\":36000, \"http2\":0}]}';
+  }
+}" >> /etc/nginx/conf.d/httpdns.conf
