9991f22a0aaff01996312580e2c733e8c63f903b
Ke Jin <jack.kejin@gmail.com>
2018-03-06 02:27:28 -0500

    Fixed restart script asking confirmations.

--- restart-unblock-server.sh
+++ restart-unblock-server.sh
@@ -1,5 +1,5 @@
 docker container stop unblock-server
-docker container prune
-docker image remove jackjinke/unblock-server
+docker container rm unblock-server -f
+docker image remove jackjinke/unblock-server -f
 docker build -t jackjinke/unblock-server .
 docker run -d -p 80:80 -p 53:53/tcp -p 53:53/udp --restart=always --cap-add=NET_ADMIN --name=unblock-server jackjinke/unblock-server
